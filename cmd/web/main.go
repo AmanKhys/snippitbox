5f5e10c8f8ffce325f6b20e0d81c57615d3aa065
AmanKhys <amankhys2112@gmail.com>
2024-09-19 21:40:19 +0530

    configure settings using flags instead of hardcoding the port/static-dir/etc using flag package and config struct to save our config

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,20 +1,32 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	type config struct {
+		addr      string
+		staticDir string
+	}
+
+	var cfg config
+
+	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
+	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
+	flag.Parse()
+
 	mux := http.NewServeMux()
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	fileServer := http.FileServer(http.Dir(cfg.staticDir))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippit/view", snippitView)
 	mux.HandleFunc("/snippit/create", snippitCreate)
 
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	log.Printf("Starting server on %s", cfg.addr)
+	err := http.ListenAndServe(cfg.addr, mux)
 	log.Fatal(err)
 }
