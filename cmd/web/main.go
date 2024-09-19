c1e951dd8b7f99fc8b978dfa308896c312280a7a
AmanKhys <amankhys2112@gmail.com>
2024-09-20 00:20:23 +0530

    use dependency injection to use our custom loggers in our handlers. - convert the handlers to methods of application struct containing our   custom logger.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,36 +7,40 @@ import (
 	"os"
 )
 
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
+
 func main() {
-	type config struct {
-		addr      string
-		staticDir string
-	}
 
-	var cfg config
-
-	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
-	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	staticDir := flag.String("static-dir", "./ui/static", "Path to static assets")
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
+
 	mux := http.NewServeMux()
 
-	fileServer := http.FileServer(http.Dir(cfg.staticDir))
+	fileServer := http.FileServer(http.Dir(*staticDir))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippit/view", snippitView)
-	mux.HandleFunc("/snippit/create", snippitCreate)
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippit/view", app.snippitView)
+	mux.HandleFunc("/snippit/create", app.snippitCreate)
 
 	srv := &http.Server{
-		Addr:     cfg.addr,
+		Addr:     *addr,
 		ErrorLog: errorLog,
 		Handler:  mux,
 	}
 
-	infoLog.Printf("Starting server on %s", cfg.addr)
+	infoLog.Printf("Starting server on %s", *addr)
 	err := srv.ListenAndServe()
 	errorLog.Fatal(err)
 }
